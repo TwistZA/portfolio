fadfc692114ddecfbf1f31e2ceda5de7685c7006
TwistZA <avi3110@gmail.com>
2021-09-26 00:27:10 +0200

    Better IMageModal left right buttons

--- src/components/ImageModal/ImageModal.js
+++ src/components/ImageModal/ImageModal.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState, useEffect } from "react";
+import styles from "./ImageModal.module.css";
 
 export default function ImageModal({ images }) {
   const [index, setIndex] = useState(0);
@@ -36,7 +37,7 @@ export default function ImageModal({ images }) {
           }}
         >
           <div>click left right to view all images</div>
-          <div>click anywhere to close image</div>
+          <div>click anywhere else to close image</div>
         </div>
       )}
 
@@ -45,11 +46,46 @@ export default function ImageModal({ images }) {
           style={{
             border: "black 1px silver",
             backgroundColor: "white",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
           }}
         >
-          <button onClick={prev}>&lt;</button>
-          <img src={images[index]} alt="" style={{ width: 360 }} />
-          <button onClick={next}>&gt;</button>
+          <button
+            className={styles.modalButton}
+            style={{
+              width: "10%",
+              fontWeight: "bold",
+              fontSize: "2rem",
+              color: "black",
+              backgroundColor: "white",
+              border: "none",
+            }}
+            onClick={prev}
+          >
+            &lt;
+          </button>
+          <img
+            src={images[index]}
+            alt=""
+            style={{
+              width: "70%",
+            }}
+          />
+          <button
+            className={styles.modalButton}
+            style={{
+              width: "10%",
+              fontWeight: "bold",
+              fontSize: "2rem",
+              color: "black",
+              backgroundColor: "white",
+              border: "none",
+            }}
+            onClick={next}
+          >
+            &gt;
+          </button>
         </div>
       )}
     </div>
